f346fc39d1838a926291714d406fb2b08e0d2c71
julTorres <julientorres3@gmail.com>
2018-05-07 12:54:36 +0200

    project initiated with pring-boot initializr. Dependencies : web, lombok, security, rest repositories, websocket, jpa, mysql, mail

---
+++ src/main/java/fr/wildcodeschool/wildslackback/WildslackbackApplication.java
@@ -0,0 +1,12 @@
+package fr.wildcodeschool.wildslackback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildslackbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildslackbackApplication.class, args);
+	}
+}
---
+++ src/test/java/fr/wildcodeschool/wildslackback/WildslackbackApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.wildcodeschool.wildslackback;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WildslackbackApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
