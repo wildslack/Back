75da81395bd0f7fc3e9b942c5c56864d0d243ad6
julTorres <julientorres3@gmail.com>
2018-05-07 16:53:06 +0200

    added user model

---
+++ src/main/java/fr/wildcodeschool/wildslackback/model/User.java
@@ -0,0 +1,23 @@
+package fr.wildcodeschool.wildslackback.model;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor @Getter @Setter
+public class User {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column (nullable = false, unique = true)
+    private String email;
+
+    @Column (nullable = false)
+    private String pseudo;
+
+}
