67d44a33b7e75f6f7cdf9e2dcc977adf40ec8858
Stephen Dupré <stephendupre64@gmail.com>
2018-05-17 15:46:44 +0200

    mise a jour du nommage du jwt en WildslackAuthorization

--- src/main/java/fr/wildcodeschool/wildslackback/security/SecurityConstants.java
+++ src/main/java/fr/wildcodeschool/wildslackback/security/SecurityConstants.java
@@ -4,5 +4,5 @@ public class SecurityConstants {
     public static final String SECRET = "wildslack.bidart@gmail.com";
     public static final long EXPIRATION_TIME = 864_000_000; // 10 jours a voir avec l'équipe wildslack
     public static final String TOKEN_PREFIX = "WildSlack ";
-    public static final String HEADER_STRING = "Authorization";
+    public static final String HEADER_STRING = "WildslackAuthorization";
 }
