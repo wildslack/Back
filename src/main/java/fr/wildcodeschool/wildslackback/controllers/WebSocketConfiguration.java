8eefc171bf8e6a786492fb76eb937468af9760cf
julTorres <julientorres3@gmail.com>
2018-06-05 15:49:43 +0200

    added WebsocketConfiguration and WebsocketController

---
+++ src/main/java/fr/wildcodeschool/wildslackback/controllers/WebSocketConfiguration.java
@@ -0,0 +1,30 @@
+package fr.wildcodeschool.wildslackback.controllers;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.support.OriginHandshakeInterceptor;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        OriginHandshakeInterceptor interceptor = new OriginHandshakeInterceptor();
+        registry.addEndpoint("/socket");
+        registry.addEndpoint("/socket")
+                .addInterceptors(interceptor)
+                .setAllowedOrigins("*")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app")
+                .enableSimpleBroker("/chat");
+    }
+}
---
+++ src/main/java/fr/wildcodeschool/wildslackback/controllers/WebSocketController.java
@@ -0,0 +1,27 @@
+package fr.wildcodeschool.wildslackback.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Controller
+public class WebSocketController {
+
+    private final SimpMessagingTemplate template;
+
+    @Autowired
+    WebSocketController(SimpMessagingTemplate template){
+        this.template = template;
+    }
+
+    @MessageMapping("/socket")
+    @SendTo("/chat/messages")//All subscribers to the “/chat/messages” destination will receive the message.
+    public void onReceivedMesage(String message){
+        this.template.convertAndSend("/chat", new SimpleDateFormat("HH:mm:ss").format(new Date())+"- "+message);
+    }
+}
