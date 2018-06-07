bca56cb651d975227ffdce8015a5302f76d756d4
Paul Ferreira Silva <paul.ferreirasilva@wildcodeschool.fr>
2018-06-07 15:03:59 +0200

    Messages API: set startMessageId optional

--- src/main/java/fr/wildcodeschool/wildslackback/controllers/MessageController.java
+++ src/main/java/fr/wildcodeschool/wildslackback/controllers/MessageController.java
@@ -23,14 +23,20 @@ public class MessageController {
 
     @RequestMapping(value = "/api/channels/{id}/messages", method = RequestMethod.GET)
     @ResponseBody
-    public List<Message> findMessagesByChannel(@PathVariable("id") long idChannel, @RequestParam("startmessageindex") long startMessageId) {
+    public List<Message> findMessagesByChannel(@PathVariable("id") long idChannel, @RequestParam(value = "startmessageid", required = false) Long startMessageId) {
         List<Message> messages = new ArrayList<>();
-        Optional<Message> message = messageRepository.findById(startMessageId);
-        if (message.isPresent()) {
-            LocalDateTime postDate = message.get().getPostDate();
-            Channel channel = channelRepository.findByIdChannel(idChannel);
-            messages = messageRepository.findFirst1000ByChannelAndPostDateBefore(channel, postDate);
+        LocalDateTime postDate = null;
+        if (startMessageId != null) {
+            Optional<Message> message = messageRepository.findById(startMessageId);
+            if (message.isPresent()) {
+                postDate = message.get().getPostDate();
+            }
+        } else {
+            postDate = LocalDateTime.now();
         }
+        Channel channel = channelRepository.findByIdChannel(idChannel);
+        messages = messageRepository.findFirst1000ByChannelAndPostDateBefore(channel, postDate);
+
         return messages;
     }
 
